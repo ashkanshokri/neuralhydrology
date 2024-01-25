c70c74b2b8660e8e624b2d4ae1412797c64a048b
Ashkan Shokri <ash.shokri@csiro.au>
2024-01-25 17:35:34 +1100

    update

--- workflows/camelaus_lstm/petrichore/run.sh
+++ workflows/camelaus_lstm/petrichore/run.sh
@@ -8,6 +8,6 @@ directory=configs/spatial_split/twofold
 # Loop through YAML files and submit sbatch jobs
 for CONFIG_FILE in $(find $directory -name "*.yml"); do
     echo $CONFIG_FILE
-    sbatch petrichore/job.slurm --export=CONFIG_FILE=$CONFIG_FILE
+    sbatch --export=CONFIG_FILE=$CONFIG_FILE petrichore/job.slurm 
     
 done
